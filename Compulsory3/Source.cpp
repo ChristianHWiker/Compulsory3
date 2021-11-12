60eec1aa0e9339d81504528b796ea90ddf2eb7fa
Christian Hagen Wiker <chrishawik@gmail.com>
2021-11-12 14:00:42 +0100

    0.1

--- Compulsory3/Source.cpp
+++ Compulsory3/Source.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <Windows.h>
+#include <conio.h>
 
 int row{};
 int col{};
@@ -11,8 +12,13 @@ char p1Token = 'X';
 std::string p2Name;
 char p2Token = 'O';
 
+std::string AI = "AI";
+bool aiON = false;
+
 void drawBoard();
 void startScreen();
+void game();
+void checkWin();
 
 
 std::string board[7][7]{ {"1","2","3","4","5","6","7"},{ "*","*","*","*","*","*","*"},{"*","*","*","*","*","*","*"},{"*","*","*","*","*","*","*"},
@@ -21,10 +27,28 @@ std::string board[7][7]{ {"1","2","3","4","5","6","7"},{ "*","*","*","*","*","*"
 void startScreen()
 {
 	std::cout << "Welcome to connect 4 !!\n\n";
+	system("pause");
+	system("cls");
 
 	std::cout << "Enter player 1 name: ";
-	
+	getline(std::cin, p1Name);
 
+	std::cout << "Enter player 2 name: ";
+	getline(std::cin, p2Name);
+
+	for (int i = 0; i < sizeof(p2Name); i++)
+	{
+		if (p2Name[i] == toupper(AI[i]))
+		{
+			bool aiON = true;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	system("cls");
 }
 
 void drawBoard()
@@ -43,9 +67,27 @@ void drawBoard()
 	}
 }
 
+void game()
+{
+	if (aiON = false)
+	{
+
+	}
+	else
+	{
+
+	}
+}
+
+void checkWin()
+{
+
+}
+
 int main()
 {
-drawBoard();
+	startScreen();
+	drawBoard();
 
 	return 0;
 }
